a2ff077bb6e17b5c746ddca144966032fc0c1b12
mung5079 <mung5079@gmail.com>
2024-05-10 17:33:35 +0900

    📦️ Css 순서 prettier 지정

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
-<script setup>
+<script setup></script>
 
-</script>
-   
 <template>
-
-
+  <div>
+  <p>앱</p>
+</div>
 </template>
 
 <style scoped>
+p {
+  flex-direction: column;
+  display: flex;
 
+  width: 100px;
+ 
+  color: red;
+  height: 100px;
+}
 </style>
