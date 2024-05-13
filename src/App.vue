1f2235a96b8b11f2ea653165154e5e8456c81eff
mung5079 <mung5079@gmail.com>
2024-05-13 18:03:42 +0900

    💄 topbar 스크롤시 사라지는 ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from "vue-router";
 import Navbar from "@/components/common/navbar/Navbar.vue";
+import Topbar from "./components/common/topbar/Topbar.vue";
 </script>
 
 <template>
   <div>
+    <Topbar />
     <Navbar />
     <p>앱</p>
     <p>앱</p>
---
+++ src/components/common/topbar/Topbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <header :class="{ scroll: isScroll }">
+    <Flex width="360px" height="56px" align="center" justify="center">
+      <Logo />
+    </Flex>
+  </header>
+</template>
+
+<script setup>
+import Flex from "@/design/Flex.vue";
+import Logo from "@/assets/svg/logo.svg";
+
+import { ref } from "vue";
+const isScroll = ref(false);
+window.addEventListener("scroll", function () {
+  isScroll.value = true;
+});
+window.addEventListener("scrollend", function () {
+  isScroll.value = false;
+});
+</script>
+
+<style scoped>
+header {
+  position: fixed;
+  top: 0;
+  background-color: white;
+}
+
+.scroll {
+  transform: translateY(-100%);
+  transition: 1s;
+}
+</style>
