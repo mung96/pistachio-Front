9b2964604737a0a971db03badb5735933d020c34
mung5079 <mung5079@gmail.com>
2024-05-13 10:51:43 +0900

    Feat: Flex 컴포넌트 구현

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -1,20 +1,20 @@
 <template>
   <nav>
-    <RouterLink :to=PATH.FEED><div class="navbar-item">피드</div></RouterLink>
-    <RouterLink :to=PATH.SEARCH><div class="navbar-item">검색</div></RouterLink>
-    <RouterLink :to=PATH.CREATE_POST><div class="navbar-item">게시글 추가</div></RouterLink>
-    <RouterLink :to=PATH.MY_PAGE><div class="navbar-item">마이페이지</div></RouterLink>
+     <Flex gap="12px">
+        <RouterLink :to=PATH.FEED><div class="navbar-item">피드</div></RouterLink>
+        <RouterLink :to=PATH.SEARCH><div class="navbar-item">검색</div></RouterLink>
+        <RouterLink :to=PATH.CREATE_POST><div class="navbar-item">게시글 추가</div></RouterLink>
+        <RouterLink :to=PATH.MY_PAGE><div class="navbar-item">마이페이지</div></RouterLink>
+      </Flex>
   </nav>
 </template>
 
 <script setup>
     import { RouterLink } from "vue-router";
     import {PATH} from "@/constants/router"
+    import Flex from "@/design/Flex.vue";
 </script>
 
 <style scoped>
-nav{
-    display: flex;
-    gap:56px;
-}
+
 </style>
---
+++ src/design/Flex.vue
@@ -0,0 +1,53 @@
+<template>
+    <div :style="flexStyle">
+        <slot> </slot>
+    </div>
+</template>
+
+<script setup>
+import { watchEffect } from 'vue';
+
+const props = defineProps({
+    direction:String,
+    gap:String,
+    justify:String,
+    align:String,
+    paddingTop:String,
+    paddingBottom:String,
+    paddingLeft:String,
+    paddingRight:String,
+    width:String,
+    height:String,
+    postition:String,
+
+})
+
+let flexStyle = {
+        display:"flex",
+        "justify-content":"center",
+        "align-items":"center",
+};
+
+watchEffect(() => {
+    flexStyle = {
+        ...flexStyle, // 기존 스타일 유지
+        display:"flex",
+        "flex-direction": props.direction,
+        "gap":props.gap,
+        "justify-content":props.justify,
+        "align-items":props.align,
+        "padding-top":props.paddingTop,
+        "padding-bottom":props.paddingBottom,
+        "padding-left":props.paddingLeft,
+        "padding-right":props.paddingRight,
+        "width":props.width,
+        "height":props.height,
+        "postition":props.postition,
+    };
+});
+
+</script>
+
+<style scoped>
+
+</style>
