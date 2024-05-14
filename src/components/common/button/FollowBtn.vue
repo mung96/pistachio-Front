16ff75f0a12e62ba232a17311e48703a4853f70f
mung5079 <mung5079@gmail.com>
2024-05-14 16:38:33 +0900

    ✨ 버튼 컴포넌트 구현

---
+++ src/components/common/button/FollowBtn.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>팔로우</button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+button {
+  border: none;
+  outline: none;
+  background-color: white;
+
+  font: var(--sm-mm-font);
+  color: var(--sub-color);
+}
+</style>
---
+++ src/components/search/Result.vue
@@ -0,0 +1,12 @@
+<template>
+  <Flex>
+    <p>프로필 사진</p>
+    <p>닉네임</p>
+  </Flex>
+</template>
+
+<script setup>
+import Flex from "@/design/Flex.vue";
+</script>
+
+<style scoped></style>
