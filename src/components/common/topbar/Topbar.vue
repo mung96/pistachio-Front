d91d2c51d5a20e1d6a98ddd1bb81c111d22ffabd
mung5079 <mung5079@gmail.com>
2024-05-14 10:11:25 +0900

    ✨ 페이지에 따라 topbar 렌더링 기능 구현

--- src/components/common/topbar/Topbar.vue
+++ src/components/common/topbar/Topbar.vue
@@ -1,7 +1,19 @@
 <template>
-  <header :class="scrollDirection">
-    <Flex width="100%" height="56px" align="center" justify="center">
-      <Logo />
+  <header v-if="type !== 'none'" :class="scrollDirection">
+    <Flex
+      width="100%"
+      height="56px"
+      align="center"
+      justify="space-between"
+      padding-top="16px"
+      padding-bottom="16px"
+      padding-right="16px"
+      padding-left="16px"
+    >
+      <ArrowLeftIcon v-if="type === 'back'" width="24" height="24" />
+      <Logo v-if="type === 'main'" />
+      <span v-if="type !== 'main'">텍스트</span>
+      <BellIcon v-if="type !== 'not-login'" width="24" height="24" />
     </Flex>
   </header>
 </template>
@@ -9,11 +21,25 @@
 <script setup>
 import Flex from "@/design/Flex.vue";
 import Logo from "@/assets/svg/logo.svg";
+import BellIcon from "@/assets/svg/bellIcon.svg";
+import ArrowLeftIcon from "@/assets/svg/arrowLeftIcon.svg";
 
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { PATH, TOP_BAR_TYPE } from "@/constants/router";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+// route에 따라 렌더링 로직 다시짜야할듯
+const type = computed(() => {
+  const key = Object.keys(PATH).findIndex((key) => PATH[key] === route.path);
+  return TOP_BAR_TYPE[Object.keys(PATH)[key]];
+});
+
 const scrollDirection = ref("");
 
 let lastScrollPosition = window.scrollY;
+
 window.addEventListener("scroll", function () {
   const curScrollPosition = window.scrollY;
   curScrollPosition - lastScrollPosition > 0
