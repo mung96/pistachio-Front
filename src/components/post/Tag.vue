1d2bbebde15d23e096dfed04a869ccd6e512ab2b
mung5079 <mung5079@gmail.com>
2024-05-21 15:31:18 +0900

    💄 태그 ui 구성

---
+++ src/components/post/Tag.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :style="{ color: color, 'border-color': color }">{{ text }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  color: String,
+  text: String,
+});
+</script>
+
+<style scoped>
+div {
+  width: fit-content;
+  border-radius: 12px;
+  padding: 10px 20px;
+  border: 2px solid;
+  font-weight: 700;
+}
+</style>
--- src/pages/post/PostPage.vue
+++ src/pages/post/PostPage.vue
@@ -3,6 +3,10 @@
     <Flex direction="column" gap="24px">
       <ImageUpload />
       <PostContent />
+      <Flex direction="column" gap="16px" padding-left="16px">
+        <Tag :color="palette.sub" text="인증글" />
+        <Tag :color="palette.main" text="프로젝트이름" />
+      </Flex>
     </Flex>
   </form>
 </template>
@@ -10,6 +14,8 @@
 import ImageUpload from "@/components/post/ImageUpload.vue";
 import PostContent from "@/components/post/PostContent.vue";
 import Flex from "@/design/Flex.vue";
+import Tag from "@/components/post/Tag.vue";
+import { palette } from "@/constants/color";
 </script>
 
 <style scoped></style>
