79b5b02056d9ae4fe810f30bb9a0498616b4d479
mung5079 <mung5079@gmail.com>
2024-05-23 11:09:55 +0900

    ✨ 프로젝트 생성 요청

--- src/apis/project/postProject.js
+++ src/apis/project/postProject.js
@@ -1,18 +1,45 @@
 import { END_POINTS } from "@/constants/api";
 import { axiosInstance } from "../axiosInstance";
 import { usePostStore } from "@/stores/post";
-
+import { useUserStore } from "@/stores/user";
 export const postProject = async () => {
   const store = usePostStore();
-  const response = await axiosInstance.post(END_POINTS.PROJECT, {
-    name: store.getProjectName(),
-    description: store.getContent(),
-    targetDonationAmount: 20000,
-    startTime: new Date(),
-    endTime: new Date(),
+  const userStore = useUserStore();
+  const formData = new FormData();
+  store.getImages().forEach((image) => {
+    console.log(image);
+    formData.append("file", image);
+    formData.append(
+      "projectRequest",
+      new Blob(
+        [
+          JSON.stringify({
+            agencyId: userStore.getUser().membershipId,
+            name: store.getProjectName(),
+            description: store.getContent(),
+            targetDonationAmount: 20000,
+            image: "",
+            startTime: new Date(),
+            endTime: new Date(),
+          }),
+        ],
+        { type: "application/json" }
+      )
+    );
   });
+
+  const response = await axiosInstance.post(END_POINTS.PROJECT, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  console.log(response);
   return response;
 };
+
+// @RequestPart("projectRequest") AddDonateProjectRequest addDonateProjectRequest,
+// @RequestPart("file") List<MultipartFile> multipartFiles
 // const formData = new FormData();
 //   store.getImages().forEach((image) => {
 //     console.log(image);
